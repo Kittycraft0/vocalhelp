f8be85e1a369155ea2baa601109cc8b60e3e23a2
Riley <iwbmo05@gmail.com>
2025-03-14 19:13:28 -0500

    spectrogram works now

--- script2.js
+++ script2.js
@@ -1,9 +1,11 @@
 // 3/14/2025
 
-var spectrumcanvas=document.getElementById("visualization");
+var spectrumcanvas=document.getElementById("spectrumvisualization");
 const spectrumctx=spectrumcanvas.getContext("2d");
-//ctx.fillStyle = 'rgb(255, 0, 0)';
-//ctx.fillRect(0,0,canvas.width,canvas.height);
+var spectrogramcanvas=document.getElementById("spectrogramcanvas");
+const spectrogramctx=spectrogramcanvas.getContext("2d");
+spectrogramctx.fillStyle = 'rgb(255, 255, 0)';
+spectrogramctx.fillRect(0,0,spectrogramcanvas.width,spectrogramcanvas.height);
 
 
 // Access the Microphone: Request permission to access the user's microphone and obtain a media stream.
@@ -31,7 +33,6 @@ navigator.mediaDevices.getUserMedia({ audio: true })
         analyser.getByteFrequencyData(dataArray);
         // dataArray now contains frequency data
     }
-
     // Extract Amplitude (Time-Domain) Data: Obtain the time-domain data to analyze the waveform.
     const timeDomainDataArray = new Uint8Array(bufferLength);
 
@@ -40,45 +41,74 @@ navigator.mediaDevices.getUserMedia({ audio: true })
         // timeDomainDataArray now contains amplitude data
     }
 
+
+    // Spectrogram
+    let drawOffset = 0;
+    function amplitudeToColor(amplitude) {
+        const value = amplitude / 255; // Normalize amplitude to [0, 1]
+        const hue = (1 - value) * 240; // Map amplitude to hue (blue to red)
+        return `hsl(${hue}, 100%, 50%)`;
+    }
+    
+    spectrogramctx.fillStyle = 'rgb(0, 0, 0)';
+    spectrogramctx.rect(50,50,50,50);
+    spectrogramctx.stroke();
+    spectrogramctx.fillRect(0,0,spectrogramcanvas.width,spectrogramcanvas.height);
+    function drawSpectrogram() {
+        requestAnimationFrame(drawSpectrogram);
+      
+        analyser.getByteFrequencyData(dataArray);
+      
+        // Scroll the image left
+        const imageData = spectrogramctx.getImageData(1, 0, spectrogramcanvas.width - 1, spectrogramcanvas.height);
+        spectrogramctx.putImageData(imageData, 0, 0);
+      
+        // Draw the new frequencies on the right edge
+        for (let i = 0; i < bufferLength; i++) {
+            const value = dataArray[i];
+            const percent = i / bufferLength;
+            const y = Math.floor(percent * spectrogramcanvas.height);
+            const color = amplitudeToColor(value);
+            spectrogramctx.fillStyle = color;
+            spectrogramctx.fillRect(spectrogramcanvas.width - 1, spectrogramcanvas.height - y, 1, 1);
+            //console.log("b");
+        }
+        console.log("aaah");
+    }
+    drawSpectrogram();
+      
+
+
+
     // Visualize the Data: Use the extracted data to create visualizations, such as frequency bars or waveforms.
     function draw() {
         requestAnimationFrame(draw);
         
         getFrequencyData();
         
-        //console.log("a");
-        // Visualization code here
+        // Spectrum visualization code here
         spectrumctx.fillStyle = 'rgb(0, 0, 0)';
         spectrumctx.fillRect(0,0,spectrumcanvas.width,spectrumcanvas.height);
         //spectrumctx.clearRect(0,0,spectrumcanvas.width,spectrumcanvas.height);
-
         let barHeight = (spectrumcanvas.height / bufferLength) * 2.5;
         let barWidth;
         let y = spectrumcanvas.height;
-        //console.log(bufferLength);
-        //var b="";
-        var min=Infinity;
-        var max=-Infinity;
         for (let i = 0; i < bufferLength; i++) {
-            if(y<min){min=y;}
-            if(y>max){max=y;}
             value = dataArray[i];
             barWidth = value/255*spectrumcanvas.width;
             spectrumctx.fillStyle = 'rgb(' + (value + 100) + ',50,50)';
-            // right justified top down
+            // right justified right to left
             //spectrumctx.fillRect(spectrumcanvas.width - barWidth / 2, y, barWidth / 2, barHeight);
-            // left justified top down
+            // left justified bottom up
             spectrumctx.fillRect(0, y, barWidth, barHeight);
-            //y -= barHeight + 1;
             y -= spectrumcanvas.height/bufferLength;
-            //b=[spectrumcanvas.width - barWidth / 2, y, barWidth / 2, barHeight];
         }
-        console.log("min: "+min+", max: "+max);
-        //try{
-        //spectrumcanvas.width-=1;
-        //console.log(b);
-        //console.log(spectrumcanvas.width);
-        //}catch(e){console.log(e);}
+
+        // Spectrogram visualization code here
+        
+        
+
+
     }
       
     draw();
@@ -111,7 +141,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
     },1000);*/
       
 
-
+    
 
 })
 .catch(function(err) {
